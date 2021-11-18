10e9328488298950ac68dc892dc183e54f361bd5
Muhammad Ridho Waradana <muhammadridhowaradana@gmail.com>
2021-11-18 14:39:36 +0700

    Kuliah 18 Nov 2021

--- routes/web.php
+++ routes/web.php
@@ -40,3 +40,17 @@ Route::post('tugasphp',"ViewController@showTugasPHP");
 //ajax load groceries
 Route::view('/grocery', 'grocery');
 Route::post('/grocery/post', 'GroceryController@store');
+
+//route CRUD
+Route::get('/pegawai','PegawaiController@index');
+Route::get('/pegawai/tambah','PegawaiController@tambah');
+
+Route::post('/pegawai/store','PegawaiController@store');
+
+Route::get('/pegawai/edit/{id}','PegawaiController@edit');
+Route::post('/pegawai/update','PegawaiController@update');
+
+Route::get('/pegawai/hapus/{id}','PegawaiController@hapus');
+
+
+
