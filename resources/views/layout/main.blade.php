11cdfd284c053d9bed70349888a7a3026d602288
Muhammad Ridho Waradana <muhammadridhowaradana@gmail.com>
2021-12-07 19:27:15 +0700

    change add layout

--- resources/views/layout/main.blade.php
+++ resources/views/layout/main.blade.php
@@ -171,7 +171,7 @@
   <!-- Main Sidebar Container -->
   <aside class="main-sidebar sidebar-dark-primary elevation-4">
     <!-- Brand Logo -->
-    <a href="index3.html" class="brand-link">
+    <a href="/" class="brand-link">
       <img src="{{ asset('dist/img/AdminLTELogo.png') }}" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
       <span class="brand-text font-weight-light">A5026201084</span>
     </a>
@@ -287,7 +287,7 @@
     <strong>Copyright &copy; 2021 <a href="https://github.com/MrWaradana">Muhammad Ridho Waradana - 5026201084 - PWEB A</a>.</strong>
     All rights reserved.
     <div class="float-right d-none d-sm-inline-block">
-      <b>Version</b> 3.1.0
+      <b>Version</b> 1.0
     </div>
   </footer>
 </div>
--- resources/views/tugas/tambah.blade.php
+++ resources/views/tugas/tambah.blade.php
@@ -5,38 +5,47 @@
 @section('judulhalaman', 'Tambah Tugas')
 
 @section('konten')
-	<a href="/tugas"> Kembali</a>
-
-
-	<form action="/tugas/store" method="post">
-		{{ csrf_field() }}
-
-        {{-- select pegawai starts here --}}
-        <label for="IDPegawai">IDPegawai:</label>
-        <select name="IDPegawai" id="IDPegawai">
-            @foreach($pegawai as $p)
-            <option value="{{ $p->pegawai_id }}">{{ $p->pegawai_nama }}</option>
-            @endforeach
-        </select>
-        {{-- select pegawai ends here --}}
-
-        <br>
-        {{-- datetime starts here --}}
-        <div class="form-group">
-            <label>Tanggal dan Waktu :</label>
-            <div class="input-group date" id="reservationdatetime" data-target-input="nearest">
-                <input type="text" class="form-control datetimepicker-input" data-target="#reservationdatetime" name="Tanggal"/>
-                <div class="input-group-append" data-target="#reservationdatetime" data-toggle="datetimepicker">
-                    <div class="input-group-text"><i class="fa fa-calendar"></i></div>
+    <div class="container">
+        <form action="/tugas/store" method="post">
+            {{ csrf_field() }}
+            <div class="form-group">
+                <label for="IDPegawai">Nama Pegawai :</label>
+                <select name="IDPegawai" id="IDPegawai" class="form-control">
+                    @foreach($pegawai as $p)
+                    <option value="{{ $p->pegawai_id }}">{{ $p->pegawai_nama }}</option>
+                    @endforeach
+                </select>
+            </div>
+            <div class="form-group">
+                <label>Tanggal dan Waktu :</label>
+                <div class="input-group date" id="reservationdatetime" data-target-input="nearest">
+                    <input type="text" class="form-control datetimepicker-input" data-target="#reservationdatetime" name="Tanggal"/>
+                    <div class="input-group-append" data-target="#reservationdatetime" data-toggle="datetimepicker">
+                        <div class="input-group-text"><i class="fa fa-calendar"></i></div>
+                    </div>
                 </div>
             </div>
-        </div>
-        {{-- datetime ends here --}}
-        <br/>
-
-		NamaTugas <input type="text" maxlength="50" name="NamaTugas" required="required"> <br/>
-		Status <input type="text" maxlength="1" name="Status" required="required"> <br/>
-		<input type="submit" value="Simpan Data">
-	</form>
-
+            <div class="form-group">
+              <label for="NamaTugas">Nama Tugas :</label>
+              <input type="text" class="form-control" name="NamaTugas" id="NamaTugas" placeholder="Nama Tugas" required="required">
+            </div>
+            <p><strong> Status</strong></p>
+            <div class="custom-control custom-radio custom-control-inline">
+                <input type="radio" id="Belum" name="Status" class="custom-control-input" value="B">
+                <label class="custom-control-label" for="Belum">Belum</label>
+            </div>
+            <div class="custom-control custom-radio custom-control-inline">
+                <input type="radio" id="Proses" name="Status" class="custom-control-input" value="P">
+                <label class="custom-control-label" for="Proses">Proses</label>
+            </div>
+            <div class="custom-control custom-radio custom-control-inline">
+                <input type="radio" id="Selesai" name="Status" class="custom-control-input" value="S">
+                <label class="custom-control-label" for="Selesai">Selesai</label>
+            </div>
+            <div class="form-group d-flex justify-content-between mt-4">
+                <a href="/tugas" class="btn btn-danger"><i class="fas fa-arrow-left"></i> Kembali</a>
+                <button type="submit" class="btn btn-success"><i class="fas fa-save"></i> Simpan Data </button>
+            </div>
+          </form>
+    </div>
 @endsection
