f9faddc393086b2c48ed39e858f0c18c531c5c47
Muhammad Ridho Waradana <muhammadridhowaradana@gmail.com>
2021-12-15 13:38:31 +0700

    minor fixes

--- resources/views/karyawan/edit.blade.php
+++ resources/views/karyawan/edit.blade.php
@@ -12,18 +12,18 @@
             {{-- <input type="hidden" name="id" value="{{ $k->karyawan_id }}"> --}}
             <div class="form-group">
                 <label for="NIP">NIP</label>
-                <input type="text" class="form-control" name="NIP" id="NIP" placeholder="NIP" required="required" value="{{ $k->NIP }}">
+                <input type="text" class="form-control" name="NIP" id="NIP" placeholder="NIP" required="required" value="{{ $k->NIP }}" maxlength="9">
               </div>
             <div class="form-group">
                 <label for="Nama">Nama</label>
                 <input type="text" class="form-control" name="Nama" id="Nama" placeholder="Nama" required="required" value="{{ $k->Nama }}">
             </div>
             <div class="form-group">
-                <label for="Pangkat">Umur</label>
+                <label for="Pangkat">Pangkat</label>
                 <input type="text" class="form-control" name="Pangkat" id="Pangkat" placeholder="Pangkat" required="required" title="masukkan angka saja!" value="{{ $k->Pangkat }}" maxlength="5">
             </div>
             <div class="form-group">
-                <label for="Gaji">Umur</label>
+                <label for="Gaji">Gaji</label>
                 <input type="text" class="form-control" name="Gaji" id="Gaji" placeholder="Gaji" required="required" title="masukkan angka saja!" value="{{ $k->Gaji }}" maxlength="5" pattern="[0-9]*" title="masukkan angka saja!">
             </div>
             <div class="form-group d-flex justify-content-between mt-4">
